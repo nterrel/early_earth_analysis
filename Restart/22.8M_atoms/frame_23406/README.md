d5a5ac230698aa20faacf407f9ef62cc25726c62
nterrel <nterrel@ufl.edu>
2024-08-20 13:51:53 -0400

    Working on quench restart of frame 196366

--- Restart/22.8M_atoms/frame_23406/README.md
+++ Restart/22.8M_atoms/frame_23406/README.md
@@ -1,1 +1,24 @@
 # Before and after quenching frame 23406 (one of the randomly selected ala-containing frames)
+
+## in.22M.quench.lammps edits:
+
+* changed 1000 steps to 100 steps
+* deleted a lot of:
+`dump           2 all dcd 50 ${log_dir}/${timestamp}_4.3ns.dcd`
+`write_data     ${log_dir}/${timestamp}_4.3ns.final`
+`write_restart  ${log_dir}/${timestamp}_4.3ns.restart`
+`run            400000`
+`undump         2`
+"""
+  commands, just want to start at 2500K and immediately decrease to 300K
+* changed initial velocities (set/determined at 0.0 K) to:
+`# Settings: temperature at 0 K`
+`velocity       all create 2500.0 12345 rot yes mom yes dist gaussian`
+`timestep       ${timestep}`
+* changed thermo frequency from 50 to 10 since I'm only running 100 steps
+
+## Molfind stuff: 
+
+* molfind_41492932 files are before quench, these could not run because of the following error:
+`RuntimeError: nonzero is not supported for tensors with more than INT_MAX elements,   file a support request`
+
--- convert_dcd_to_data.py
+++ convert_dcd_to_data.py
@@ -5,7 +5,7 @@ from top_loader import load_topology
 from ase.io import read, write
 
 topology = load_topology('traj_top_0.0ns.h5')
-trajectory = md.load('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_23406/frame_23406_0.2ns_original.dcd', top=topology)
+trajectory = md.load('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_196366/frame_196366_2.4ns_original.dcd', top=topology)
 
 # Correct the unit cell dimensions (converting nm to Å)
 cell_lengths_nm = trajectory.unitcell_lengths
@@ -15,9 +15,9 @@ trajectory.unitcell_lengths = cell_lengths_angstroms
 frame_index = 0
 frame = trajectory[frame_index]
 
-frame.save('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_23406/frame_23406.xyz')
+frame.save('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_196366/frame_196366.xyz')
 
-atoms = read('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_23406/frame_23406.xyz')
+atoms = read('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_196366/frame_196366.xyz')
 atoms.set_cell(cell_lengths_angstroms[0])
 
-write('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_23406/frame_23406.data', atoms, format='lammps-data')
+write('/red/roitberg/nick_analysis/Restart/22.8M_atoms/frame_196366/frame_196366.data', atoms, format='lammps-data')
