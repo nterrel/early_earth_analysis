393c4c7d81fee4af12aba0661497ac00e5420695
nterrel <nterrel@ufl.edu>
2024-09-04 19:20:19 -0400

    Created a (template) shell script for iterating over a list of directories and run `dcd2data.py` to make the workflow a bit less tedious.
    Also created a `split_some_frames.py` script to create dcd files for the remaining frames (non-ala specific frames, such as most molecules, most formulas, and the two frames with very large molecules found)
    
    WIP but want to check out for the day, will probably make some errors tomorrow so here's a checkpoint.

---
+++ dcd2data_on_dir.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --job-name=dcd2data
+#SBATCH --ntasks=1
+#SBATCH --mem=120gb
+#SBATCH --time=04:00:00
+#SBATCH --output=/red/roitberg/nick_analysis/dcd2data_%j.log
+#SBATCH --output=dcd2data_%j.out       # Output file
+#SBATCH --error=dcd2data_%j.err        # Error file
+
+directories=(
+    "frame_23406",
+    "frame_24221",  # NEED TO CREATE FRAME FOR THIS DIR
+    "frame_39901",
+    "frame_48938",
+    "frame_53726",
+    "frame_71744",
+    "frame_102584",
+    "frame_127761",
+    "frame_164418",
+    "frame_166471",
+    "frame_224713",
+    "frame_256780",  # NEED TO CREATE FRAME FOR THIS DIR
+    "frame_256786",  # NEED TO CREATE FRAME FOR THIS DIR
+    "frame_290169",
+    "frame_344041"   # NEED TO CREATE FRAME FOR THIS DIR
+)
---
+++ split_some_frames.py
@@ -0,0 +1,4 @@
+# NOTE: Adapt `split_traj.py` and `split_ala_frames.py` into a script which can isolate the following frames:
+# 24221, 256780, 256786, 344041
+
+# GET TIMESTAMPS FROM PANDAS DATAFRAME (SOMEWHERE...)
