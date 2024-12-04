9943ad5c498325061fe3ff3388b166483a872777
nterrel <nterrel@ufl.edu>
2024-12-04 13:02:08 -0500

    Fixed the few failed restarted 'unique fragment' molfind analyses, moved the files to the proper location. Now need to use (most likely, dask) some script to combine parquet files to a single merged_mol, merged_formula pair.

--- unique_fragment_analysis/submit_unique_molfind.sh
+++ unique_fragment_analysis/submit_unique_molfind.sh
@@ -1,3 +1,5 @@
+# This calls a python script within the lammps-ani code to write analysis scripts and batch submit them -- if any fail, you'll have to specify the 'segment_index' for the failed segment and restart it with that argument in the molfind command (otherwise it will have incorrect frame / time numbering)
+
 python /blue/roitberg/nterrel/lammps-ani/cumolfind/submit_analysis.py \
     --traj=/red/roitberg/22M_20231222_prodrun/ \
     --top=/blue/roitberg/apps/lammps-ani/examples/early_earth/data/mixture_22800000.pdb \
