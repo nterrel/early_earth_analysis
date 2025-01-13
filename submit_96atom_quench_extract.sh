069844aaf44f4eeabd11b01072db4922562a5239
nterrel <nterrel@ufl.edu>
2025-01-13 13:56:07 -0500

    molfind test script added, put a notes line in the 96 atom extract script

--- submit_96atom_quench_extract.sh
+++ submit_96atom_quench_extract.sh
@@ -4,6 +4,8 @@
 #SBATCH --mem=32gb
 #SBATCH --time=48:00:00
 
+#NOTE: THIS MUST BE RAN IN THE `single_mol_analysis` BRANCH OF LAMMPS-ANI
+
 module load conda
 conda activate /blue/roitberg/apps/torch1121
 python /red/roitberg/nick_analysis/extract_96atom_quench.py &> /red/roitberg/nick_analysis/extract_96atom_quench.txt
